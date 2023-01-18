fc8441c37eaab2624346eb56974b569cf0c75507
Sachin <sachindevop@gmail.com>
2023-01-18 10:04:13 -0800

    add code for app.js

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.send('<h1>Auto Scaling Demo App</h1> <h4>Message: Success</h4> <p>version: 1.0.0</p>');
 })
 
+app.get('/test', (req, res) => {
+    res.send('<h1>test API</h1> <h4>Message: Success</h4>');
+})
+
 app.listen(port,()=>{
     console.log(`demo app is up and listening to port ${port}`);
 })
