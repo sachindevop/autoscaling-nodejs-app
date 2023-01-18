53397af962ed58658e5c0208775037a7058acaef
Sachin devop <109357704+sachindevop@users.noreply.github.com>
2023-01-18 11:02:58 -0800

    Update user-data-for-template.sh

--- user-data-for-template.sh
+++ user-data-for-template.sh
@@ -1,28 +1,10 @@
-#!/bin/bash -ex
-# output user data logs into a separate file for debugging
-exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
-# download nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
-# source nvm
-. /.nvm/nvm.sh
-# install node
-nvm install node
-#export NVM dir
-export NVM_DIR="/.nvm"	
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"	
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" 
-#upgrade yum
-sudo yum upgrade
-#install git
+#!/bin/bash
+yum update -y
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install v8.9.3
 sudo yum install git -y
-cd /home/ec2-user
-# get source code from githubt
 git clone https://github.com/sachindevop/autoscaling-nodejs-app.git
-#get in project dir
 cd autoscaling-nodejs-app
-#give permission
-sudo chmod -R 755 .
-#install node module
 npm install
-# start the app
-node app.js > app.out.log 2> app.err.log < /dev/null &
+node app.js
