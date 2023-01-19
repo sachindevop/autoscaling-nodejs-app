5f1bb3d6c6957429c1b63bcf0fe67a7768b793b3
Sachin devop <109357704+sachindevop@users.noreply.github.com>
2023-01-19 03:19:05 -0800

    Update user-data-for-template.sh

--- user-data-for-template.sh
+++ user-data-for-template.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-yum update -y
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
+sudo yum update -y
+sudo curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install v8.9.3
 sudo yum install git -y
 git clone https://github.com/sachindevop/autoscaling-nodejs-app.git
 cd autoscaling-nodejs-app
 npm install
-node app.js > /opt/nodejs_app/node.log &
+nohup node app.js &
