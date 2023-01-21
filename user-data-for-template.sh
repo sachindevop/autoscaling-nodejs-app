76ad2b0010ede8b14c08db5257a70cb8436b842a
Sachin devop <109357704+sachindevop@users.noreply.github.com>
2023-01-21 02:34:01 -0800

    Update user-data-for-template.sh

--- user-data-for-template.sh
+++ user-data-for-template.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 sudo yum update -y
-sudo curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install v8.9.3
-sudo yum install git -y
-git clone https://github.com/sachindevop/autoscaling-nodejs-app.git
-cd autoscaling-nodejs-app
-npm install
-sudo cp ./srever.sh /etc/systemd/system/appjs.service
+sudo yum install -y gcc-c++ make
+curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash -
+sudo yum install -y nodejs
+sudo yum install -y git
+sudo git clone https://github.com/sachindevop/autoscaling-nodejs-app.git
+cd /autoscaling-nodejs-app
+sudo cp ./server.sh /etc/systemd/system/appjs.service
 sudo systemctl enable appjs.service
 sudo systemctl start appjs.service
+sudo npm install
